5c54d015d5b397168288b87eec5111967d470233
Ben Pham <benjamin.pham@gmail.com>
2023-02-28 18:21:47 +0100

    Simple login button

--- spec/system/authenticates_users_spec.rb
+++ spec/system/authenticates_users_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "AuthenticatesUsers", type: :system do
   before(:each) { OmniAuth.config.mock_auth[:github] = nil }
 
   before do
-    OmniAuth.config.test_mode = true
     OmniAuth.config.add_mock(:github, FactoryBot.create(:auth_hash, :github))
   end
 
